890407503c1451e6ee07f4ee0242f41664f05f40
kmcconeghy <kevinmcconeghy@gmail.com>
2017-11-20 14:17:44 -0500

    tgp edit

--- NEWS.md
+++ NEWS.md
@@ -2,6 +2,6 @@
 
 Project set-up, data upload.
 
-# Version 0.0.2 
+# Version 0.0.2 10-26-2017  
 
 GitPages / Vignettes Set-up
--- R/flumodelr.R
+++ R/flumodelr.R
@@ -3,10 +3,15 @@
 #' 
 #' This package contains multiple vignettes describing datasources, 
 #' background and examples.
-#'
+#' 
+#' @section Functions:
+#' 
+#' 
 #' @section Vignettes:
-#' Introduction: A general introduction to package 
+#' Introduction: A general introduction to package  
 #' testdata: A description of example data sources
+#' 
+#' @section Important Citations:
 #' 
 #' @docType package
 #' @name flumodelr
