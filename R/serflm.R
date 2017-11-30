68a4238078f7b66f4bd176f561a56ca8ea372cbf
kmcconeghy <kevinmcconeghy@gmail.com>
2017-11-30 15:19:56 -0500

    tpg

--- R/serflm.R
+++ R/serflm.R
@@ -3,21 +3,22 @@
 #' @description Performs a cyclical linear regression model, 
 #' or 'serfling' model
 #'
-#' @param 
+#' @param data A dataframe class object
+#' 
 #' @return an object of class lm.summary
+#' 
 #' @export
+#' 
 #' @examples
 #' require(flumodelr)
-#' cdc122 <- flumodelr::cdc122
-#' m.out <- serflm(treat ~ age + educ + black + hispan, 
-#'                 data=cdc122)
+#' cdc122 <- flumodelr::cdc122city
+#' m.out <- serflm(data=cdc122)
 #' summary(m.out)
-#'
-#'
-
 serflm <- function(data=NULL) {
+  
   #sanity checks
   
   #return results
   return(2+2)
+  
 }
