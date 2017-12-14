3ae81b6ad22f4d792ee063205c7e19b69b5a93d5
kmcconeghy <kevinmcconeghy@gmail.com>
2017-12-14 12:09:02 -0500

    editing serfling vignette

--- vignettes/03-serfling.R
+++ vignettes/03-serfling.R
@@ -11,16 +11,32 @@ library(scales)
 library(flumodelr)
 library(tibble)
 
-## ---- results='asis', fig.width=7----------------------------------------
+## ---- echo=F, results='asis', fig.width=7--------------------------------
 flu_ex <- flumodelr::flu_ex
 
 g1 <- ggplot(flu_ex, aes(x=yrweek_dt, y=fludeaths)) + 
   geom_line(size=0.8, colour="#CC0000") +
   scale_x_date(labels = date_format("%Y"), date_breaks="1 year",
                expand=c(0, .9)) + 
-  xlab("Year-month") + 
+  xlab("Year") + 
   ylab("Deaths per 100,000") + 
-  theme_light(base_size=16)
+  theme_light(base_size=16) +
+  theme(plot.title = element_text(size=14)) +
+  labs(title="Figure 1. Pneumonia and Influenza Deaths Over Time")
+g1
+
+## ---- echo=F, results='asis', fig.width=7--------------------------------
+g1 <- ggplot(flu_ex, aes(x=yrweek_dt, y=fludeaths)) + 
+  geom_line(size=0.8, colour="#CC0000") +
+  geom_smooth(method='lm') +
+  scale_x_date(labels = date_format("%Y"), date_breaks="1 year",
+               expand=c(0, .9)) + 
+  xlab("Year") + 
+  ylab("Deaths per 100,000") + 
+  theme_light(base_size=16) +
+  theme(plot.title = element_text(size=14)) +
+  labs(title="Figure 2. Pneumonia and Influenza Deaths Over Time",
+       caption="Ordinary Least Squares")
 g1
 
 ## ---- results='as.is', fig.width=7---------------------------------------
@@ -30,30 +46,24 @@ fit <- flu_ex %>%
          theta = 2*week2/52,
          sin_f1 = sinpi(theta),
          cos_f1 = cospi(theta),
-         week_2 = week2^2,
-         week_3 = week2^3,
-         week_4 = week2^4,
-         week_5 = week2^5,
          pred_y = predict( lm(fludeaths ~ 
-                                week2 + sin_f1 + cos_f1, data=.)),
-         pred_ypoly = predict( lm(fludeaths ~ week2 + 
-                                week_2 + week_3 + week_4 + week_5 +
-                                sin_f1 + cos_f1, data=.)))
+                                week2 + sin_f1 + cos_f1, data=.)))
 
 ## ---- echo=F, results='as.is', fig.width=7-------------------------------
 g1 <- ggplot(fit, aes(x=yrweek_dt)) + 
   geom_line(aes(y=fludeaths), size=0.8, colour="#CC0000") +
   geom_line(aes(y=pred_y), size=0.8, colour="black") +
-  geom_line(aes(y=pred_ypoly), size=0.8, linetype=2, colour="blue") +
   scale_x_date(labels = date_format("%Y"), date_breaks="1 year",
                expand=c(0, .9)) + 
   xlab("Year") + 
   ylab("Deaths per 100,000") + 
-  theme_light(base_size=16)
+  theme_light(base_size=16) +
+  theme(plot.title = element_text(size=14)) +
+  labs(title="Figure 3. Pneumonia and Influenza Deaths Over Time",
+       caption="Serfling Model")
 g1
 
 ## ------------------------------------------------------------------------
-cat("Example data")
 flu_ex %>% select(yrweek_dt, fludeaths)
 
 ## ------------------------------------------------------------------------
@@ -88,14 +98,13 @@ df_pred <- flu_ex_serfling %>%
           interval="prediction", level=0.95)
 
 pred_y0 <- df_pred$fit[,1] #fitted values
-pred_y0_uci <- df_pred$fit[,3] #95% UCI
 pred_y0_serf <- df_pred$fit[,1] + 1.64*sd(df_pred$fit[,1]) 
 
 df_base <- flu_ex %>%
-  add_column(., pred_y0, pred_y0_uci, pred_y0_serf)  
+  add_column(., pred_y0, pred_y0_serf)  
 
 df_base %>% select(year, week, fludeaths, 
-                   pred_y0, pred_y0_uci, pred_y0_serf)  
+                   pred_y0, pred_y0_serf)  
 
 ## ---- echo=F, results='as.is', fig.width=7-------------------------------
 ggplot(df_base, aes(x=yrweek_dt)) + 
@@ -103,28 +112,22 @@ ggplot(df_base, aes(x=yrweek_dt)) +
                 linetype="Observed Deaths"), size=0.8) +
   geom_line(aes(y=pred_y0, colour="Predicted Deaths", 
                 linetype="Predicted Deaths"), size=0.8) +
-  geom_line(aes(y=pred_y0_uci, colour="95% PI", 
-                linetype="95% PI"), size=0.8) +
   geom_line(aes(y=pred_y0_serf, colour="Serfling Threshold", 
                 linetype="Serfling Threshold"), size=0.8) +
   scale_colour_manual("Line",
                       breaks=c("Observed Deaths", 
                                  "Predicted Deaths", 
-                                 "95% PI",
                                  "Serfling Threshold"),
                       values = c("Observed Deaths"="#CC0000", 
                                  "Predicted Deaths"="black", 
-                                 "95% PI"="black",
                                  "Serfling Threshold"="black")) +
   scale_linetype_manual("Line", 
                         breaks=c("Observed Deaths", 
                                  "Predicted Deaths", 
-                                 "95% PI",
                                  "Serfling Threshold"),
                         values = c("Observed Deaths"=1,
                                    "Predicted Deaths"=1,
-                                   "95% PI"=2, 
-                                   "Serfling Threshold"=3)) +
+                                   "Serfling Threshold"=2)) +
   scale_x_date(labels = date_format("%Y"), date_breaks="1 year",
                expand=c(0, .9)) + 
   xlab("Year") + 
@@ -145,7 +148,7 @@ df_epi <- df_base %>%
   rowwise() %>%
   mutate(epidemic = if_else(sum(epidemic, epidemic2, epidemic3, 
                                    epidemic4, epidemic5, na.rm=T)>0, T, F),
-         excess = if_else(epidemic==T & (fludeaths - pred_y0_uci)>0, fludeaths - pred_y0_uci, 0))
+         excess = if_else(epidemic==T & (fludeaths - pred_y0_serf)>0, fludeaths - pred_y0_serf, 0))
 df_epi %>% select(year, week, threshold, epidemic, excess)
 
 ## ---- echo=F, results='as.is', fig.width=7.0-----------------------------
